5f8c68a2a8627f635915d070432a989b8fccae0e
omery1 <omery1223@gmail.com>
2023-09-02 16:09:30 +0300

    high score added

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ scoreboard = Scoreboard()
 
 
 screen.listen()
-
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
 
 
 game_is_on = True
@@ -40,18 +39,13 @@ while game_is_on:
 
     #with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
-
-
-
-
+            scoreboard.reset()
+            snake.reset()
 
 
 
